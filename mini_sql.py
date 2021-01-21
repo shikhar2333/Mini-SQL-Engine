3f8efa5dc11bea2777593022c12c65ffdc8ed35a
shikhar2333 <shikh5030@gmail.com>
2021-01-21 15:27:40 +0530

    group by, order by implemented

--- mini_sql.py
+++ mini_sql.py
@@ -3,10 +3,9 @@ import sys
 import re
 import itertools
 
-DB_DIR = "files/"
-META_FILE = "files/metadata.txt"
-AGGREGATE = ["min", "max", "sum", "avg", "count"]
-RELATE_OPS = ["<=", ">=", "<", ">", "="]
+TABLE_DIR = "files/"
+METAFILE = "files/metadata.txt"
+RELATIONAL_OPS = ["<=", ">=", "<", ">", "="]
 db_schema = {}
 CONST = "INTCONST"
 
@@ -27,7 +26,7 @@ def isint(s):
         return False
 
 def ReadDbSchema():
-    fp = open(META_FILE, "r")
+    fp = open(METAFILE, "r")
     schema_contents = fp.readlines()
     cur_table = None
     for line in schema_contents:
@@ -66,7 +65,7 @@ def IsOr(s):
 def IsRelOp(op):
     flag = True
     rel_op = ''
-    for opr in RELATE_OPS:
+    for opr in RELATIONAL_OPS:
         if opr in op:
             flag = False
             rel_op = opr
@@ -100,12 +99,12 @@ def HandleRelop(op, take_cols, cols, cond_idx):
     
     return take_cols
     
-def OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op):
+def OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op, groupby_dict, orderby_col, is_dist):
     tables_needed = [[] for _ in output_tables]
     cross_join_idx = {}
-    cnt = 0
+    count = 0
     for idx, table in enumerate(output_tables):
-        file_name = DB_DIR + table + '.csv'
+        file_name = TABLE_DIR + table + '.csv'
         get_cols = ReadCsv(file_name)
         for i,row in enumerate(get_cols):
             rows = []
@@ -114,17 +113,15 @@ def OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op):
                 rows.append(row[col_idx])
             tables_needed[idx].append(rows)   
 
-        for i, cname in enumerate(cols_needed[table]):
-            if table in cross_join_idx.keys():
-                cross_join_idx[table].update({cname: cnt + i})
-            else:
-                cross_join_idx[table] = {cname: cnt + i}
-        cnt += len(cols_needed[table])
+        cross_join_idx[table] = {c_name: count+i for i, c_name in enumerate(cols_needed[table])}
+        count = count + len(cols_needed[table])
 
+    # Cartesain product of the tables
     inter_table = [[i for tup in r for i in tup] for r in itertools.product(*tables_needed)]
-    # for t in inter_table:
-    #     print(t)
     final_table = []
+
+
+    # execute where clause
     if output_cond != []:
         take_cols = [[True for _ in range(len(output_cond))] for _ in range(len(inter_table))]
         for cond_idx, cond in enumerate(output_cond):
@@ -155,14 +152,80 @@ def OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op):
                 continue
             choose_cols = []
             for j, (t_name, c_name, _) in enumerate(output_cols):
-                idx = cross_join_idx[t_name][c_name]
-                choose_cols += [inter_table[i][idx]]
+                n = len(t_name)
+                for k in range(n):
+                    idx = cross_join_idx[t_name[k]][c_name[k]]
+                    choose_cols += [inter_table[i][idx]]
             final_table.append(choose_cols)           
-        for t in final_table:
-            print(t)
-    
-    # print(cross_join_idx)
-    if output_cols[0][2]:
+        # for t in final_table:
+        #     print(t)
+
+    # execute group by clause
+    if groupby_dict['groupby_table']:
+        groupby_table = groupby_dict['groupby_table']
+        proj_col = cross_join_idx[groupby_table][groupby_dict['proj_col']]
+        aggr_cols = groupby_dict['aggr_cols']
+        seen = {}
+        if final_table != []:
+            inter_table = final_table.copy()
+        final_table = []
+        proj_col_idx = None
+        def find_proj_idx(proj_col_idx):
+            for j, (t_name, c_name, aggr) in enumerate(output_cols):
+                for k, cname in enumerate(c_name):
+                    if cname == groupby_dict['proj_col']:
+                        proj_col_idx = j
+                        break
+                if proj_col_idx:
+                    break
+            return proj_col_idx
+
+        proj_col_idx = find_proj_idx(proj_col_idx)
+        for j, (t_name, c_name, aggr) in enumerate(output_cols):
+            for k, cname in enumerate(c_name):
+                rows = []
+                if cname == groupby_dict['proj_col']:
+                    continue
+                for i, row in enumerate(inter_table):
+                    val = row[proj_col_idx]  
+                    if val not in seen.keys():
+                        seen[val] = {} 
+                    if cname not in seen[val].keys():
+                        seen[val][cname] = []
+
+                    seen[val][cname].append((row[j], aggr))
+
+        def dict_helper():
+            for key, val in seen.items():
+                rows = []
+                for key1, val1 in val.items():
+                    temp = [itr for itr, _ in val1]
+                    aggr = val1[0][1]
+                    if len(rows) == proj_col_idx:
+                        rows += [key]
+                    if aggr == "min":
+                        rows += [min(temp)]
+                    elif aggr == "max":
+                        rows += [max(temp)]
+                    elif aggr == "sum":
+                        rows += [sum(temp)]
+                    elif aggr == "average":
+                        rows += [sum(temp)/len(temp)]
+                    elif aggr == "count":
+                        rows += [len(temp)]
+                    else:
+                        raise_error("Aggregrate function is invalid")
+                
+                final_table.append(rows)
+        
+        dict_helper()
+
+    aggr_list = [a for _, _, a in output_cols]
+    # execute simple aggregrate functions
+    if _all(aggr_list):
+        if final_table != []:
+            inter_table = final_table.copy()
+        final_table = []
         for idx, (t_name, c_name, aggr) in enumerate(output_cols):
             cols = []
             for row_idx, row in enumerate(inter_table):
@@ -177,19 +240,106 @@ def OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op):
                 final_table.append(max(cols))
             elif aggr == "count":
                 final_table.append(len(cols))
-            elif aggr == "distinct":
-                n = len(t_name)
-                cols = [[] for _ in range(len(inter_table))]    
-                for row_idx, row in enumerate(inter_table):
-                    for j in range(n):
-                        col_idx = cross_join_idx[t_name[j]][c_name[j]]
-                        cols[row_idx] += [row[col_idx]]
-                for row_idx, row in enumerate(inter_table):
-                    if row not in final_table:
-                        final_table.append(row)
             else:
                 raise_error("Invalid aggregrate function")
-        print(final_table)
+    
+    # execute distinct clause
+    if is_dist:
+        if final_table != []:
+            inter_table = final_table.copy()
+        final_table = []
+        for row_idx, row in enumerate(inter_table):
+            rows = []
+            for col_idx, col_val in enumerate(row):
+                rows.append(col_val)
+            if rows not in final_table:
+                final_table.append(rows)
+
+    # execute order by clause
+    if orderby_col[0]:
+        orderby_table = None
+        col_order_idx = None
+        if final_table == []:
+            final_table = inter_table.copy()
+        def find_orderby_idx(col_order_idx):
+            for j, (t_name, c_name, aggr) in enumerate(output_cols):
+                for k, cname in enumerate(c_name):
+                    if cname == orderby_col[0]:
+                        col_order_idx = j
+                        break
+                if col_order_idx:
+                    break
+            return col_order_idx
+
+        col_order_idx = find_orderby_idx(col_order_idx)
+        final_table = sorted(final_table, key = lambda x: x[col_order_idx])
+        if orderby_col[1] == "desc":
+            final_table = final_table[::-1]
+        elif orderby_col[1] != "asc":
+            raise_error("Invalid order by option: Use asc/desc")
+
+    if final_table == []:
+        final_table = inter_table.copy()
+    # print(final_table)
+    col_names = []
+    for j, (t_name, c_name, aggr) in enumerate(output_cols):
+        for k, cname in enumerate(c_name):
+            if aggr:
+                col_names.append(aggr + '(' + cname + ')')
+            else:
+                col_names.append(t_name[k] + '.' + cname)
+    # print(col_names)
+    return final_table, col_names
+
+def OrderByParser(orderby_col, cols_list):
+    if orderby_col == []:
+        return [None]
+    f = True
+    col_ord = orderby_col[0]
+    regmatch = re.match("(.+)\((.+)\)", col_ord)
+    AssertCond(len(orderby_col) == 1, "Missing asc/desc option in order by clause")
+    if regmatch:
+        aggr_func, col_ord = regmatch.groups()
+    for col in cols_list:
+        regmatch = re.match("(.+)\((.+)\)", col)
+        if regmatch:
+            aggr_func, col = regmatch.groups()
+        if col == col_ord:
+            f = False
+            break
+    AssertCond(f, "No projection column found corresponding to the given order by column")
+    return [col_ord, orderby_col[1]]
+    
+def GroupByParser(groupby_list, tables_list, cols_list):
+    if groupby_list == []:
+        return None, None
+    col_to_grp = groupby_list[1]
+    flag = True
+    groupby_table = None
+    for table in tables_list:
+        if col_to_grp in db_schema[table]:
+            groupby_table = table
+            break
+    AssertCond(groupby_table == None, "No matching table for the given group by column")
+    for col in cols_list:
+        if col_to_grp == col:
+            flag = False
+            break
+    AssertCond(flag, "No projection column found corresponding to the given group by column")
+    flag = True
+    aggr_cols = []
+    for col in cols_list:
+        regmatch = re.match("(.+)\((.+)\)", col)
+        if regmatch:
+            aggr_func, col = regmatch.groups()
+        else:
+            aggr_func = None
+        if aggr_func:
+            aggr_cols += [col]
+    AssertCond(len(aggr_cols)==0, "No aggregrate function found for the group by clause")
+    
+    return groupby_table, aggr_cols
+
 def CondParser(cond_list, tables_list):
     output_cond = []
     cond_op = None
@@ -203,7 +353,6 @@ def CondParser(cond_list, tables_list):
         cond_list, cond_op = cond_list.split(" or "), "or"
     else:
         cond_list = [cond_list]
-    # print(cond_list)
     for cond in cond_list:
         left, right, rel_op = IsRelOp(cond)
         expr_list = [left, right]
@@ -221,7 +370,6 @@ def CondParser(cond_list, tables_list):
                 cond_parse.append([tnames[0], col_name])
 
         output_cond.append(cond_parse)
-    # print(output_cond, cond_op)
     return output_cond, cond_op
     
 def TableParser(tables_list):
@@ -231,7 +379,8 @@ def TableParser(tables_list):
         AssertCond(table not in db_schema.keys(), "Table name '{}'absent in schema".format(table))
         output_tables += [table]
 
-    return output_tables
+    return # for t in final_table:
+        #     print(t)output_tables
 
 def _any(iterable):
     for itr in iterable:
@@ -247,48 +396,26 @@ def _all(iterable):
 
 def ColoumnParser(tables_list, cols_list):
     output_cols = []
-    # print(cols_list)
     for col in cols_list:
-        # print(col)
         regmatch = re.match("(.+)\((.+)\)", col)
         if regmatch:
             aggr_func, col = regmatch.groups()
         else:
             aggr_func = None
-        # print(aggr_func, col)
         tname = []
-        if col != "*" and aggr_func != "distinct":
+        if col != "*":
             for t in tables_list:
                 if col in db_schema[t]:
                     tname += [t]
             AssertCond(len(tname) > 1, "Column Name not unique")
             AssertCond(tname == [], "Column Name doesn't exist in schema")
             output_cols += [(tname, [col], aggr_func)]
-        elif col != "*":
-            col = col.split(',')
-            for t in tables_list:
-                for coloumn in col:
-                    if coloumn in db_schema[t]:
-                        tname += [t]
-            # print(col,tname)
-            AssertCond(len(tname) != len(col), "Some column names don't exist in the schema")
-            output_cols += [(tname, col, aggr_func)]
         else:
             AssertCond(aggr_func != None, "Aggregrate function can't be used with *")
             for t in tables_list:
                 for c in db_schema[t]:
                     output_cols += [([t], [c], aggr_func)]
     
-    aggr_list = []
-    for _, _, aggr in output_cols:
-        aggr_list += [aggr]
-    AssertCond(_all(aggr_list)^_any(aggr_list), "Aggregrated coloumns can't be used with non-aggregrate ones")
-    def is_distinct(iterable):
-        for itr in iterable:
-            if itr == "distinct":
-                return True
-        return False
-    AssertCond(is_distinct(aggr_list) and len(aggr_list)!=1, "Distinct keyword can't be used with other coloumns")
     return output_cols
 
 def QueryParser(q):
@@ -304,7 +431,6 @@ def QueryParser(q):
     groupby_index = []
     orderby_index = []
     n = len(q)
-    # print(q)
     index = 0
     while index < n:
         val = q[index]
@@ -353,11 +479,17 @@ def QueryParser(q):
                 cond = cond or groupby_index[0] > orderby_index[0]
                 AssertCond(cond, "Query is Invalid")
                 groupby_list += q[groupby_index[0] + 1: orderby_index[0]]
-                orderby_list += q[orderby_index[0] + 1:]
+                orderby_list += q[orderby_index[1] + 1:]
             else:
                 groupby_list += q[groupby_index[0] + 1:]
         else:
-            cond_list += q[where_index[0] + 1:]
+            if orderby_index:
+                cond = cond or where_index[0] > orderby_index[0]
+                AssertCond(cond, "Query is Invalid")
+                cond_list += q[where_index[0] + 1: orderby_index[0]]
+                orderby_list += q[orderby_index[1] + 1:]
+            else:
+                cond_list += q[where_index[0] + 1:]
     else:
         if groupby_index:
             cond = groupby_index[0] < from_index[0]
@@ -366,10 +498,10 @@ def QueryParser(q):
             if orderby_index:
                 cond = cond or groupby_index[0] > orderby_index[0]
                 AssertCond(cond, "Query is Invalid")
-                groupby_list += q[groupby_index[1] + 1: orderby_index[0]]
+                groupby_list += q[groupby_index[0] + 1: orderby_index[0]]
                 orderby_list += q[orderby_index[1] + 1:]
             else:
-                groupby_list += q[groupby_index[1] + 1:]
+                groupby_list += q[groupby_index[0] + 1:]
         else:
             if orderby_index:
                 cond = orderby_index[0] < from_index[0]
@@ -380,64 +512,71 @@ def QueryParser(q):
                 tables_list += q[from_index[0] + 1:]
     
     cols_list = q[select_index[0] + 1:from_index[0]]
-    # print(tables_list, cond_list, groupby_list, orderby_list, cols_list)
-    return tables_list, cols_list, cond_list, groupby_list, orderby_list    
+    return tables_list, cols_list, cond_list, groupby_list, orderby_list   
     
 def pre_process_query(q):
-    out_q = []
     q = q.strip()
     if q[-1] == ';':
         q = q[:len(q) - 1]
+    else:
+        raise_error("Semicolon missing at end")
+    
     q = q.lower().split()
-    dis_idx = -1
-    from_idx = -1
+    dis_idx = []
     for i,keyword in enumerate(q):
         if keyword == "distinct":
-            dis_idx = i
-        if keyword == "from":
-            from_idx = i
-    cols = '('
-    from_idx = -1 if dis_idx == -1 else from_idx
-    for i in range(dis_idx + 1, from_idx):
-        cols += q[i]
-    cols += ')'
-    i = 0
-    n = len(q)
-    while i < n:
-        if q[i] == "distinct":
-            out_q.append(q[i] + cols)
-            i = from_idx
-            continue
-        out_q.append(q[i])
-        i += 1
-    return " ".join(out_q)
+            dis_idx += [i]
 
+    AssertCond(len(dis_idx) > 1, "Distinct keyword appears more than once") 
+    if dis_idx != []:
+        q = q[:dis_idx[0]] + q[dis_idx[0] + 1:]
+    return " ".join(q), dis_idx
+
+def get_output(final_table, col_names):
+    print(*col_names, sep=', ')
+    for i, row in enumerate(final_table):
+        print(*row, sep=', ')
 def main():
     q = ''
+    is_distinct = None
     if len(sys.argv) == 2:
         q = sys.argv[1]
-        q = pre_process_query(q)
-        print(q)
+        q, is_distinct = pre_process_query(q)
+        # print(q)
     else:
         raise_error("Invalid Query format")
     ReadDbSchema()
     p = QueryParser(q)
     tables_list = "".join(p[0]).split(',')
     output_tables = TableParser(tables_list)
-    output_cols = ColoumnParser(tables_list, p[1])
-    print(output_cols)
-    output_cond, cond_op = CondParser( p[2], tables_list)
-    print(output_cond)
-    cols_needed = {t : set() for t in output_tables}
+    cols_list = "".join(p[1]).split(',')
+    output_cols = ColoumnParser(tables_list, cols_list)
+    output_cond, cond_op = CondParser(p[2], tables_list)
+    cols_needed = {t : [] for t in output_tables}
     for table, col, aggr in output_cols:
         for i in range(len(table)):
-            cols_needed[table[i]].add(col[i])
+            cols_needed[table[i]].append(col[i])
     
     for cond in output_cond:
         cond = cond[1:]
         for table, val in cond:
             if table != CONST:
-                cols_needed[table].add(val)
+                if val in cols_needed[table]:
+                    continue
+                cols_needed[table].append(val)
 
-    OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op)
+    groupby_table, aggr_cols = GroupByParser(p[3], tables_list, cols_list)
+    orderby_col = OrderByParser(p[4], cols_list)
+    groupby_dict = {}
+    groupby_dict['groupby_table'] = None
+    groupby_dict['aggr_cols'] = None
+    groupby_dict['proj_col'] = None
+    if groupby_table:
+        groupby_dict['groupby_table'] = groupby_table
+        groupby_dict['aggr_cols'] = aggr_cols
+        groupby_dict['proj_col'] = p[3][1]
+    
+    is_dist = (len(is_distinct) == 0)
+    final_table, col_names =  OutputTable(output_tables, output_cols, cols_needed, output_cond, cond_op, groupby_dict, orderby_col, is_dist)
+    get_output(final_table, col_names)
 main()
